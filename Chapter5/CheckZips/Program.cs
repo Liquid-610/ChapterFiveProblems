bfaf51dcb4a5fe1b00a3dfbfa9cca513840b93c0
Liquid-610 <jamesbarnes71@gmail.com>
2014-03-27 09:46:39 -0400

    first done

---
+++ Chapter5/CheckZips/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CheckZips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] zips = new int[10];
+            Console.Write("Enter a Zipcode: ");
+            String input = Console.ReadLine();
+
+            for (int i = 0; i < zips.Length; i++)
+            {
+            
+
+            }
+
+            
+        }
+    }
+}
